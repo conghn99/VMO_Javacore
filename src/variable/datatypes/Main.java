685a5a16f46b56627558f688324ec864eff4f044
c99 <c99@gmail.com>
2023-03-20 10:54:06 +0700

    variable + datatype

--- src/variable/datatypes/Main.java
+++ src/variable/datatypes/Main.java
@@ -1,10 +1,8 @@
+package variable.datatypes;
+
 public class Main {
     public static void main(String[] args) {
-//        2
-//        - Variables
-//        - Data Types
-//        - Working with array
-//        - Control Flow with if-else, switch case
+        // Primitive type
         byte numByte = 1;
         short numShort = 10;
         int numInt = 1000;
@@ -13,5 +11,8 @@ public class Main {
         double numDouble = 10000d;
         char word = 'a';
         boolean isTrue = true;
+
+        // Reference type: class, interface, collection, string, arr,..
+        String str = "this is a string";
     }
 }
---
+++ src/variable/datatypes/Variable.java
@@ -0,0 +1,14 @@
+package variable.datatypes;
+
+public class Variable {
+    // Biến instance
+    public String name;
+    public int age;
+    public String address;
+
+    public static void main(String[] args) {
+        //Biến cục bộ
+        int x = 10;
+        System.out.println(x);
+    }
+}
